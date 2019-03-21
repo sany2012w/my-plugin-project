f4e168d672ec9f3fc482069b08a5d10f9a7790cf
alexa <sany2012w@gmail.com>
2019-03-21 11:42:09 +0300

    hello_2

--- src/main/java/ExecutionTime.java
+++ src/main/java/ExecutionTime.java
@@ -1,2 +1,34 @@
-public class ExecutionTime {
+import com.intellij.execution.*;
+import com.intellij.execution.process.ProcessHandler;
+import com.intellij.execution.runners.ExecutionEnvironment;
+import com.intellij.openapi.components.ProjectComponent;
+import com.intellij.util.messages.MessageBusConnection;
+import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.project.Project;
+
+import static com.intellij.openapi.util.Clock.getTime;
+
+public class ExecutionTime implements ProjectComponent {
+
+    private long startTime = 0;
+
+    public ExecutionTime(@NotNull Project project){
+        MessageBusConnection busConnection = project.getMessageBus().connect();
+        busConnection.subscribe(ExecutionManager.EXECUTION_TOPIC, new ExecutionListener() {
+            @Override
+            public void processStarted(@NotNull String executorId, @NotNull ExecutionEnvironment env, @NotNull ProcessHandler handler) {
+                startTime = getTime();
+            }
+
+            @Override
+            public void processTerminated(
+                    @NotNull String executorId, @NotNull ExecutionEnvironment env,
+                    @NotNull ProcessHandler handler, int exitCode) {
+                float exTime = getTime() - startTime;
+            }
+        });
+
+    }
+
+
 }
